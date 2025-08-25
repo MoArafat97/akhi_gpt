1d2953a3240e31438f1b5144ff9872889d91c391
MoArafat97 <khotarafat@gmail.com>
2025-08-25 12:53:28 +0100

    Fix dependency conflicts and build issues: Replace test_coverage with coverage package, add encrypt package, remove broken test files, fix Gradle build syntax

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.moarafat.nafs_ai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23  // Required for Isar database and fragment shaders
+        minSdk = flutter.minSdkVersion  // Required for Isar database and fragment shaders
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
